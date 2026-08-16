26c2f3a98ca9e3935b583392d7946536e263b3a0
btech6013123-star <btech60131.23@bitmesra.ac.in>
2026-08-16 17:00:14 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.use("/history", historyrroutes);
 app.use("/comment", commentroutes);
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`server running on port ${PORT}`);
 });
 
