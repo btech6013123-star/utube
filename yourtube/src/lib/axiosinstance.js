c73aaf92546c456bfeb747106745d2c0fcd187d9
btech6013123-star <btech60131.23@bitmesra.ac.in>
2026-08-16 16:51:21 +0530

    Update axiosinstance.js

--- yourtube/src/lib/axiosinstance.js
+++ yourtube/src/lib/axiosinstance.js
@@ -1,11 +1,7 @@
 import axios from "axios";
 
-const backendUrl =
-  process.env.NEXT_PUBLIC_BACKEND_URL ||
-  process.env.BACKEND_URL ;
-
 const axiosInstance = axios.create({
-  baseURL: backendUrl,
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
 });
 
 export default axiosInstance;
